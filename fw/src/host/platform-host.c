96f359cbc8a62b5ef7f24139a022754bb3b7343f
Jonas Norling <jonas.norling@gmail.com>
2015-08-15 17:41:37 +0200

    Build rules for host

---
+++ fw/src/host/platform-host.c
@@ -0,0 +1,15 @@
+#include "platform.h"
+#include "codec.h"
+
+void platformInit(void)
+{
+}
+
+void platformMainloop(void)
+{
+}
+
+void codecRegisterProcessFunction(CodecProcess fn)
+{
+    (void)fn;
+}

d35031358c717d2427229e52e786d6676bca87ea
Jonas Norling <jonas.norling@gmail.com>
2018-03-24 13:10:44 +0100

    Sawsynth: volume and filter envelopes

--- fw/src/sawsynth/sawsynth.c
+++ fw/src/sawsynth/sawsynth.c
@@ -18,6 +18,23 @@ struct wtOscCtx {
     const float* subtable;
 };
 
+struct envelopeState {
+    int attack; /// in samples
+    int decay; /// in samples
+    float sustain; /// level
+    int release; /// in samples
+    enum {
+        ENV_STATE_ATTACK,
+        ENV_STATE_DECAY,
+        ENV_STATE_SUSTAIN,
+        ENV_STATE_RELEASE,
+        ENV_STATE_FINISHED,
+    } state;
+    int counter;
+    float value; /// current level
+    float releaseValue; /// value when key was released
+};
+
 struct voice {
     bool active;
     uint8_t note;
@@ -25,6 +42,7 @@ struct voice {
     struct wtOscCtx oscs[3];
     FloatBiquadCoeffs filterCoeffs;
     FloatBiquadState filterState;
+    struct envelopeState envelopeState[2];
 };
 
 struct sequencerEvent {
@@ -42,6 +60,7 @@ static const struct sequencerEvent events[] = {
     { 0x2d, 70, 0 },
     { 0x30, 72, 80 },
     { 0x38, 72, 0 },
+
     { 0x40, 58, 120 },
     { 0x48, 58, 0 },
     { 0x50, 62, 100 },
@@ -70,6 +89,7 @@ static void noteOn(uint8_t note, uint8_t velocity)
     for (size_t i = 0; i < ARRAY_SIZE(voices); i++) {
         if (!voices[i].active) {
             voice = &voices[i];
+            break;
         }
     }
     if (!voice) {
@@ -85,29 +105,42 @@ static void noteOn(uint8_t note, uint8_t velocity)
     voice->oscs[1].subtable = wtSaw[0];
     voice->oscs[2].hz = NOTE2HZ(note);
     voice->oscs[2].subtable = wtSaw[0];
-    bqMakeLowpass(&voice->filterCoeffs, HZ2OMEGA(40 + velocity * 80), 0.3);
+    voice->envelopeState[0].attack = 48 * 10;
+    voice->envelopeState[0].decay = 48 * 10;
+    voice->envelopeState[0].sustain = 1.0;
+    voice->envelopeState[0].release = 48 * 100;
+    voice->envelopeState[1].attack = 48 * 100;
+    voice->envelopeState[1].decay = 48 * 3000;
+    voice->envelopeState[1].sustain = 0.24;
+    voice->envelopeState[1].release = 48 * 40;
     voice->active = true;
 }
 
 static void noteOff(uint8_t note)
 {
     for (size_t i = 0; i < ARRAY_SIZE(voices); i++) {
-        if (voices[i].active && voices[i].note == note) {
-            voices[i].active = false;
+        if (voices[i].active && voices[i].note == note &&
+            voices[i].envelopeState[0].state != ENV_STATE_RELEASE) {
+            voices[i].envelopeState[0].state = ENV_STATE_RELEASE;
+            voices[i].envelopeState[0].counter = 0;
+            voices[i].envelopeState[0].releaseValue = voices[i].envelopeState[0].value;
+            voices[i].envelopeState[1].state = ENV_STATE_RELEASE;
+            voices[i].envelopeState[1].counter = 0;
+            voices[i].envelopeState[1].releaseValue = voices[i].envelopeState[1].value;
         }
     }
 }
 
 static void stepSequencer()
 {
-    static unsigned frameCount;
-    frameCount++;
+    static unsigned sampleCount = 0;
+    sampleCount += CODEC_SAMPLES_PER_FRAME;
 
-    if (frameCount % 8) {
+    if (sampleCount % 512) {
         return;
     }
 
-    static uint8_t substep;
+    static uint8_t substep = -1;
     substep = (substep + 1) % 0x80;
 
     for (size_t i = 0; i < ARRAY_SIZE(events); i++) {
@@ -119,6 +152,47 @@ static void stepSequencer()
                 noteOff(events[i].note);
             }
         }
+    }
+}
+
+static void stepEnvelope(struct envelopeState* env, unsigned samples)
+{
+    env->counter += samples;
+
+    switch (env->state) {
+    case ENV_STATE_ATTACK:
+        if (env->counter < env->attack) {
+            env->value = (float)env->counter / env->attack;
+        }
+        else {
+            env->counter = 0;
+            env->value = 1.0;
+            env->state = ENV_STATE_DECAY;
+        }
+        break;
+    case ENV_STATE_DECAY:
+        if (env->counter < env->decay) {
+            env->value = 1.0 - (1.0 - env->sustain) * (float)env->counter / env->decay;
+        }
+        else {
+            env->counter = 0;
+            env->value = env->sustain;
+            env->state = ENV_STATE_SUSTAIN;
+        }
+        break;
+    case ENV_STATE_SUSTAIN:
+        break;
+    case ENV_STATE_RELEASE:
+        if (env->counter < env->release) {
+            env->value = env->releaseValue - env->releaseValue * (float)env->counter / env->release;
+        }
+        else {
+            env->value = 0;
+            env->state = ENV_STATE_FINISHED;
+        }
+        break;
+    case ENV_STATE_FINISHED:
+        break;
     }
 }
 
@@ -135,6 +209,15 @@ static void process(const AudioBuffer* restrict in, AudioBuffer* restrict out)
             continue;
         }
 
+        stepEnvelope(&voice->envelopeState[0], CODEC_SAMPLES_PER_FRAME);
+        stepEnvelope(&voice->envelopeState[1], CODEC_SAMPLES_PER_FRAME);
+        bqMakeLowpass(&voice->filterCoeffs, HZ2OMEGA(40 + voice->envelopeState[1].value * voice->velocity * 80), 0.3);
+
+        if (voice->envelopeState[0].state == ENV_STATE_FINISHED) {
+            voice->active = false;
+            continue;
+        }
+
         float buf1[CODEC_SAMPLES_PER_FRAME] = {};
 
         for (size_t o = 0; o < ARRAY_SIZE(voice->oscs); o++) {
@@ -147,8 +230,8 @@ static void process(const AudioBuffer* restrict in, AudioBuffer* restrict out)
 
         FloatAudioBuffer filterin, filterout;
         for (unsigned s = 0; s < CODEC_SAMPLES_PER_FRAME; s++) {
-            filterin.s[s][0] = buf1[s];
-            filterin.s[s][1] = buf1[s];
+            filterin.s[s][0] = voice->envelopeState[0].value * buf1[s];
+            filterin.s[s][1] = voice->envelopeState[0].value * buf1[s];
         }
         bqProcess(&filterin, &filterout, &voice->filterCoeffs, &voice->filterState);
 
